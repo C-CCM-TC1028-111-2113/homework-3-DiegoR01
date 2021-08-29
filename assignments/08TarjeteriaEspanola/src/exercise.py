f7aba7eef0aa33ab0bb6e050174e96386207c19b
DiegoR01 <88850966+DiegoR01@users.noreply.github.com>
2021-08-29 18:48:47 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,18 @@
+def Calbanene ():
+    return albanene * 12
 
+def Cplumones (): 
+    return plumones * 35
+
+def total ():
+    return Cplumones/ Calbanene
 def main():
     #escribe tu código abajo de esta línea
+    plumones = input("dame la cantidad de plumones")
+albanene = input ("dame la cantidad de papel ")
+
+print ("el numero maximo de tarjetas que se puede hacer es de :",total)
+    
     pass
 
 if __name__=='__main__':
