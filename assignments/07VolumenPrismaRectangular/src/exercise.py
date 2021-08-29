4e22e4741c490f8c61b672cf498735d77a0b1ced
DiegoR01 <88850966+DiegoR01@users.noreply.github.com>
2021-08-29 18:29:39 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,12 @@
-
+def volumentotal (base,altura,profundidad):
+    return base*altura*profundidad
 def main():
     #escribe tu código abajo de esta línea
+    base = input ("dame la base: " )
+altura = input ("dame la altura: ")
+profundidad = input ("dame la profundidad: ")
+
+print ("el volumen total del prisma es:", volumentotal)
     pass
 
 if __name__=='__main__':
