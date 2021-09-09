c3ce3e3d446444cb8485d002f5892575e338b810
Christelle <christelle@itesm.mx>
2021-09-09 08:22:55 -0500

    Revisar comentarios en ejercicios.

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -2,11 +2,12 @@ def volumentotal (base,altura,profundidad):
     return base*altura*profundidad
 def main():
     #escribe tu código abajo de esta línea
-    base = input ("dame la base: " )
+    base = input ("dame la base: " ) ##Tenias que converit a entero
+##Indentar el código correctamente.
 altura = input ("dame la altura: ")
 profundidad = input ("dame la profundidad: ")
 
-print ("el volumen total del prisma es:", volumentotal)
+print ("el volumen total del prisma es:", volumentotal) ##Llamar la función con los parámetros requeridos entre paréntesis
     pass
 
 if __name__=='__main__':
--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,17 +1,17 @@
 def Calbanene ():
-    return albanene * 12
+    return albanene * 12 #cuál es el valor de albanene, al estar en main, no es una variable global
 
 def Cplumones (): 
-    return plumones * 35
+    return plumones * 35  #plumnos no es una variable global, debiste pasarlas como parámetros
 
 def total ():
-    return Cplumones/ Calbanene
+    return Cplumones/ Calbanene ##Llamado de funciones con paréntesis
 def main():
     #escribe tu código abajo de esta línea
     plumones = input("dame la cantidad de plumones")
 albanene = input ("dame la cantidad de papel ")
 
-print ("el numero maximo de tarjetas que se puede hacer es de :",total)
+print ("el numero maximo de tarjetas que se puede hacer es de :",total) ##Llamado de funciones con paréntesis
     
     pass
 
